1c207236ddfc132cb77cb1d30203c7eb6cc2cfde
Daniel Randall <daniel@danielrandall.org>
2013-09-23 16:33:57 -0700

    Stopped annoying sounds. Required SHIFT modifier flag.

--- CodeTree/CodeTree/CodeTree/TheView.m
+++ CodeTree/CodeTree/CodeTree/TheView.m
@@ -32,24 +32,32 @@
     // Drawing code here.
 }
 
+// currently accepting only "{" and "}" as input:
 -(void)keyDown:(NSEvent *)theEvent
 {
 	CodeTreeAppDelegate* d = NSApplication.sharedApplication.delegate;
 	
-	if ([theEvent keyCode] == kVK_ANSI_LeftBracket)
+	unsigned short keyCode = [theEvent keyCode];
+	NSUInteger modifierFlags = [theEvent modifierFlags];
+
+	if (modifierFlags & NSShiftKeyMask)
 	{
-		[d.theLevelIndicator setIntValue:[d.theLevelIndicator intValue] + 1];
-		
-		NSLog(@"\n open \n");		
-	}
-	else if ([theEvent keyCode] == kVK_ANSI_RightBracket)
-	{
-		[d.theLevelIndicator setIntValue:[d.theLevelIndicator intValue] - 1];
-		
-		NSLog(@"\n close \n");		
+		if (keyCode == kVK_ANSI_LeftBracket)
+		{
+			[d.theLevelIndicator setIntValue:[d.theLevelIndicator intValue] + 1];
+			
+			NSLog(@"\n open \n");		
+		}
+		else if (keyCode == kVK_ANSI_RightBracket)
+		{
+			[d.theLevelIndicator setIntValue:[d.theLevelIndicator intValue] - 1];
+			
+			NSLog(@"\n close \n");		
+		}
 	}
 	
-	[super keyDown:theEvent];
+	// calling this superclass method makes annoying sounds; so not doing it:
+	//[super keyDown:theEvent];
 }
 
 -(BOOL)canBecomeKeyView
